7f8b67094ddb75c22faf8fe1dd9c81d7a51c35cc
pcjitsu <45546763+pcjitsu@users.noreply.github.com>
2023-06-17 15:33:04 -0500

    Update script.js
    Correct Script JS, need to create new pull

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,39 @@
-// Assignment code here
-var writePassword = function () {
-  let passwordLength = Number(
-    prompt("Please Select A Number Between 8 and 128")
-  );
-  if (passwordLength < 8 || passwordLength > 128) {
+// Assignment code here\
+
+//Creates a loop to continue to prompt user until password length is acceptable
+var possibleValues = {
+  lowerCaseArray: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  upperCaseArray: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  numberArray: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+  specialArray: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"],
+};
+
+var generatePassword = function () {
+  let passwordLength = Number(prompt("Please Select A Number Between 8 and 128"));
+  while (passwordLength < 8 || passwordLength > 128) {
     passwordLength = Number(prompt("Please Select A Number Between 8 and 128"));
   }
+  checkValues();
 };
+
+//Get Arrays prepped for pool of values
+var passwordValuePool = {};
+
+//Start adding values to pool with checks
+var checkValues = function () {
+  var lowerArrayCheck = prompt("Do you want to include lower case letters? \n Type Y or N").toLowerCase();
+  if ((lowerArrayCheck = "Y")) {
+    passwordValuePool += possibleValues.lowerCaseArray;
+  }
+  var upperArrayCheck = prompt("Do you want to include lower case letters? \n Type Y or N").toLowerCase();
+  if ((upperArrayCheck = "Y")) {
+    passwordValuePool += possibleValues.upperCaseArray;
+  }
+  console.log(passwordValuePool);
+};
+
+//Thinking of array within array and cycling until password is complete
+//2 Ways to solve; remove value pairs on no or add value pairs to array
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -24,9 +51,9 @@ generateBtn.addEventListener("click", writePassword);
 
 // PSUEDO FOR ASSIGNMENT//
 
-// 1-Write Code that Promps user on button click
-// 2-prompt for length of password between 8-128
-// 3- Validate that 8-128 was entered, otherwise reject and start Over
+// 1-Write Code that Promps user on button click Done
+// 2-prompt for length of password between 8-128 Done
+// 3- Validate that 8-128 was entered, otherwise reject and start Over Done
 // 4-prompts with a series of Y or No to include the different types
 // 5- cycle through different arrays until target pasword length is met
 // 6- display output on screen
