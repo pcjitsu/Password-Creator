78729afed0377a1d42cbfa87ab7a1a27845b745a
pcJitsu <antonios.zikos1@gmail.com>
2023-06-20 21:16:10 -0500

    Add README updates, image of project, cleaned up comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,7 @@
-// Assignment code here\
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-//Creates a loop to continue to prompt user until password length is acceptable
+//Creating origial object with all possible values
 var possibleValues = {
   lowerCaseArray: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
   upperCaseArray: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
@@ -12,16 +11,15 @@ var possibleValues = {
 
 //Get Arrays prepped for pool of values
 var passwordValuePool = {};
-// var passwordLength = "";
 
 //Checking to ensure numbers are in the required length and also that its a number
 var generatePassword = function () {
   var passwordLength = prompt("Please Select A Number Between 8 and 128");
-  console.log("Result: ", passwordLength);
+  //Creates a loop to continue to prompt user until password length is acceptable
   while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) === true) {
     passwordLength = Number(prompt("Please Select A Number Between 8 and 128"));
   }
-
+  //Runs the Check Values Function While passing local variable of passwordLength
   checkValues(passwordLength);
 };
 
@@ -43,11 +41,9 @@ var checkValues = function (passwordLength) {
   if (specialArrayCheck == true) {
     passwordValuePool.special = possibleValues.specialArray;
   }
-
+  //Calls Character check while continuing to pass local variable
   characterCheck(passwordLength);
 };
-// checkValues();
-console.log(passwordValuePool);
 
 //Checking if characters were selected
 var characterCheck = function (passwordLength) {
@@ -55,76 +51,28 @@ var characterCheck = function (passwordLength) {
     alert("You must choose a character type, please try again by clicking the button");
   }
 
-  //Pulling random valuoe from object
+  //Ensures that new object has values; meaning some character type has been selected
   if (Object.values(passwordValuePool).length !== 0) {
+    //Continues passing local variable and calls createPassword
     createPassword(passwordLength);
   }
 };
-
-// var randomObectValue = function () {
-//   var keyIndex = Math.floor(Math.random() * Object.values(passwordValuePool).length);
-//   var keyValue = Object.values(passwordValuePool)[keyIndex];
-//   var valueIndex = Math.floor(Math.random() * keyValue.length);
-//   var indexPassword = keyValue[valueIndex];
-//   console.log(indexPassword);
-// };
-
-// characterCheck();
 
 // Actually start creating the password by looping through the object
 var passwordCreated = "";
 var createPassword = function (passwordLength) {
   while (passwordCreated.length < passwordLength) {
-    // randomObectValue();
     var keyIndex = Math.floor(Math.random() * Object.values(passwordValuePool).length);
     var keyValue = Object.values(passwordValuePool)[keyIndex];
     var valueIndex = Math.floor(Math.random() * keyValue.length);
     var indexPassword = keyValue[valueIndex];
-    console.log(indexPassword);
     passwordCreated += indexPassword;
-    console.log(passwordCreated);
   }
 
+  //Display text to website
   var passwordText = document.querySelector("#password");
   passwordText.value = passwordCreated;
 };
-//return on generatePassword()
-
-// Write password to the #password input
-// function writePassword() {
-//   // var password = generatePassword();
-//   // var passwordText = document.querySelector("#password");
-
-//   // passwordText.value = password;
-//   generatePassword();
-// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
-
-// PSUEDO FOR ASSIGNMENT//
-
-// 1-Write Code that Promps user on button click Done
-// 2-prompt for length of password between 8-128 Done
-// 3- Validate that 8-128 was entered, otherwise reject and start Over Done
-// 4-prompts with a series of Y or No to include the different types; and confirm Done
-// 5-check that at least one character type has been selected Done
-// 6- cycle through different arrays until target pasword length is met
-// 7- display output on screen
-
-// Challenge for Assignment
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-//  THEN the password is either displayed in an alert or written to the page
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Password Generator Starter Code
+
+# Description
+
+This webpage can be utilized to generate a secure password utilizing 4 different character types.
+Futhermore, this shows my ability in understanding how to utilize the following concepts in Javascript:
+
+1. If Statements
+2. While Loops
+3. Array and Object Data Structures
+4. Methods
+5. Passing local variables and utilizing global variables
+6. Use Web APIs to crete prompts and conirmations
+
+## Installation
+
+Go to Website; No installation required
+If no characters are selected during prompt, please refresh page.
+
+## Usage
+
+How to use this app is to go to website, compare to acceptance criteria, click generate button, make selections of at least 1 character type and check output
+
+## Credits
+
+SMU Bootcamp Resources
+StacoverFlow Reources
+
+## License
+
+MIT License
+
+---
+
+## Tests
+
+Click button and run through prompts
+
+Go to website and compare to mock up; please look at screenshot
+![WebAppScreenshot](Assets/images/Password-Creator-Mock.png?raw=true)
